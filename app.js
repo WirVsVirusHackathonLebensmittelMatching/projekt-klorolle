db909338404408eecca1a64789ca8a0c9b55c977
Martin Dreier <martin@martindreier.de>
2020-03-21 11:14:59 +0100

    Add frontend folder

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//Frontend
+app.use("/webapp", express.static(__dirname + "/webapp"));
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
