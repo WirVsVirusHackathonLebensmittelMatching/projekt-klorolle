48d7b001ff7386a366bf35dbfc38d9092e4199ca
Anton <anton@ju60.de>
2020-03-21 21:38:23 +0100

    add db seeding

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,8 +1,19 @@
+const fs = require('fs');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const path = require('path');
 
 const dbPath = path.join(__dirname, '..', 'data', 'db.json');
+const dbSeedPath = path.join(__dirname, '..', 'data', 'db-seed.json');
+
+if (process.env.NODE_ENV !== 'production') {
+  try {
+    fs.statSync(dbPath);
+  } catch (e) {
+    // not database found copy sample data
+    fs.copyFileSync(dbSeedPath, dbPath);
+  }
+}
 
 const adapter = new FileSync(dbPath);
 const db = low(adapter);
@@ -10,7 +21,7 @@ const db = low(adapter);
 // Set some defaults (required if your JSON file is empty)
 db.defaults({
   shops: [],
-  shopGoods: [],
+  goods: [],
   timeslots: [],
 }).write();
 
