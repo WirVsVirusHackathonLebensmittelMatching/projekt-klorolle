65be0cd299c0a361f3baf3ea1af8992fd2267142
Anton <anton@ju60.de>
2020-03-22 11:11:03 +0100

    add api info endpoint

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,8 +1,32 @@
 const path = require('path');
 const AutoLoad = require('fastify-autoload');
+const git = require('git-last-commit');
+const { version } = require('../../../package.json');
 
 module.exports = (fastify, opts, done) => {
-  fastify.get('/', async () => ({ hello: 'Welcome to Projekt-Klorolle! (api version 1)' }));
+  fastify.get('/', {
+    schema: {
+      summary: 'api info',
+    },
+  }, () => new Promise((resolve, reject) => {
+    git.getLastCommit((err, commit) => {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve({
+        hello: 'Welcome to Projekt-Klorolle! (api version 1)',
+        version,
+        git: {
+          hash: commit.hash,
+          branch: commit.branch,
+          message: commit.subject,
+          author: commit.author.name,
+          date: new Date(commit.committedOn * 1000),
+        },
+      });
+    });
+  }));
 
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'services'),
